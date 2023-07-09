3225e10a600cd576551ac6c35b61abfc1eab4d5d
Prashant-here <88961056+Prashant-here@users.noreply.github.com>
2023-07-09 18:59:59 +0530

    Update aggressiveCows.cpp

--- aggressiveCows.cpp
+++ aggressiveCows.cpp
@@ -1,3 +1,5 @@
+// Problem of Hard Binary Search 
+
 #include <bits/stdc++.h>         // TC O(n*m) SC O(1)   
 
 using namespace std;
@@ -29,4 +31,52 @@ int main() {
     }
   }
   cout << "The largest minimum distance is " << ans << '\n';
+}
+
+
+// Using Binary Search 
+// O(n*logm)       SC O(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+bool canWePlace(vector<int> arr,int mid,int k)
+{
+     int cntcows=1;
+    
+     int lastPlacedCows=arr[0];
+     for(int i=1;i<arr.size();i++)
+     {
+        if(arr[i]-lastPlacedCows>=mid)
+        {
+            cntcows++;
+            lastPlacedCows=arr[i];
+        }
+     }
+     if(cntcows>=k) return true;
+     else return false;
+}
+int findAggressiveCows(vector<int> arr,int size,int k)
+{
+    sort(arr.begin(),arr.end());
+    int low=0,high=arr[size-1]-arr[0];
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(canWePlace(arr,mid,k)==true)
+        {
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return high;
+}
+int main()
+{
+    vector<int> arr={0,3,4,7,9,10};
+    int size=arr.size();
+    int k=4;
+    cout<<findAggressiveCows(arr,size,k);
+    return 0;
 }
